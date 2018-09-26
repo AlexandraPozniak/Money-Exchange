f0e0ebfcd49102fd65e7fb1927ea4a333c6c6acb
Art Ser <artser97@yandex.ru>
2018-09-26 18:03:29 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+module.exports = function makeExchange(currency) {//55
+  var half_dollar = (currency / 50);//0.xxx
+  var quarter_dollar = (currency%50)/25;//0,xxx
+  var dime = ((currency%50)%25)/10;//0.1
+  var nickel = (((currency%50)%25)%10)/5;
+  var penny = currency%5;
+  var output = {};
+  if (half_dollar>=1) {
+    output.H = Math.floor(half_dollar);
+  };
+  if (quarter_dollar>=1) {
+    output.Q = Math.floor(quarter_dollar);
+  }
+  if (dime>=1) {
+    output.D = Math.floor(dime);
+  };
+  if (nickel>=1) {
+    output.N = Math.floor(nickel);
+  };
+  if (penny>=1) {
+    output.P = Math.floor(penny);
+  };
+
+  if (currency === 0) {
+    return {};
+  }
+  else if (currency > 10000) {
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  }
+  else {
+    return output;
+  };
 }
